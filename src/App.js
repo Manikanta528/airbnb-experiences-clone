10bd1f12c0d83f96376c9e9e434214c8e8e47735
Manikanta528 <chmanikanta528@gmail.com>
2022-10-30 13:48:08 +0530

    app js added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Card from "./components/Card";
+import HeroComponent from "./components/HeroComponent";
+import Navbar from "./components/Navbar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Data from "./data"
+
+export default function App(){
+    const Cards = Data.map(data => 
+         <Card
+            key = {data.id}
+            item = {data}
+        />
+    )
+    return(
+        <div>
+            <Navbar/>
+            <HeroComponent/>
+            <section className="cards-flex">
+                {Cards}
+            </section>
+        </div>
+    )
 }
-
-export default App;
